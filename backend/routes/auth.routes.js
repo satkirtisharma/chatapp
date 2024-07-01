7a5cb96e6ff126532d05b60374f9c8dbcb673ef3
satkirtisharma <sksharma5864@gmail.com>
2024-07-01 12:06:15 +0530

    1st part Completed - backend

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,8 @@
+import express  from "express";
+import cookieParser from 'cookie-parser';  
+import { login, logout, signup } from "../controllers/auth.controller.js";
+const router= express.Router();
+router.post('/signup',signup);
+router.post('/login',login);
+router.post('/logout',logout);
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from "./routes/user.routes.js";
+import messageRoutes from './routes/message.routes.js';
+import cookieParser from 'cookie-parser';  
+import connectToMongoDB from './db/connectToMongoDB.js';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth/', authRoutes);
+app.use('/api/messages/', messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+
+// app.get('/', (req, res) => {
+//     res.send("Hello ji"); 
+// });
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server is running on port ${PORT}`);
+});
