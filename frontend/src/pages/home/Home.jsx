f23eddbaba079d23b5b66752909c39f0ace47726
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2025-12-14 17:49:04 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,18 +3,25 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className="h-screen flex flex-col md:flex-row rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-			
-			{/* Sidebar */}
-			<div className="w-full md:w-72">
+		<div
+			className="
+				h-screen 
+				flex flex-col md:flex-row 
+				rounded-lg 
+				overflow-hidden 
+				overscroll-none
+				bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0
+			"
+		>
+			{/* ===== Sidebar ===== */}
+			<div className="w-full md:w-72 h-full overflow-y-auto border-b md:border-b-0 md:border-r border-slate-500">
 				<Sidebar />
 			</div>
 
-			{/* Chat Area */}
-			<div className="flex-1 w-full">
+			{/* ===== Chat Area ===== */}
+			<div className="flex-1 w-full h-full overflow-hidden">
 				<MessageContainer />
 			</div>
-
 		</div>
 	);
 };
