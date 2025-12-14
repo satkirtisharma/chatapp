7c5aa411d744c73a486c65f5c062bb8d5698eea2
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2025-12-14 17:38:24 +0530

    Update Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,10 +3,20 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
+		<div className="h-screen flex flex-col md:flex-row rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+			
+			{/* Sidebar */}
+			<div className="w-full md:w-72">
+				<Sidebar />
+			</div>
+
+			{/* Chat Area */}
+			<div className="flex-1 w-full">
+				<MessageContainer />
+			</div>
+
 		</div>
 	);
 };
+
 export default Home;
