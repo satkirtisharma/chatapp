870371929b76db5259569aba7f3b9617a088f5f6
Satkirti Sharma <98018041+satkirtisharma@users.noreply.github.com>
2025-12-14 13:36:00 +0530

    Fix CORS for frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,21 +14,25 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const __dirname = path.resolve();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+/* 🔥 CORS */
+app.use(
+	cors({
+		origin: [
+			"https://chatapp-frontend-opal-six.vercel.app",
+			"http://localhost:5173",
+		],
+		credentials: true,
+	})
+);
+
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-// Commenting frontend static serve temporarily
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
 
 server.listen(PORT, () => {
 	connectToMongoDB();
